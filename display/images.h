504a7c586b89b9178aecbfac9d3cefe3b5c50b66
Michelle S <m.shen@mail.utoronto.ca>
2024-11-27 12:24:30 -0500

    Move images to separate file

---
+++ display/images.h
@@ -0,0 +1,10 @@
+#ifndef IMAGES_H
+#define IMAGES_H
+
+extern const int DINO[8][8];
+extern const int DINO_SCENE[48][60];
+extern const int DINO_SCENE_1[16][60];
+extern const int DINO_SCENE_2[16][60];
+extern const int DINO_SCENE_3[16][60];
+
+#endif
